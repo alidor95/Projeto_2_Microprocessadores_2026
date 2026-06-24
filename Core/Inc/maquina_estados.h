51f14a108d35a322403d9426c517ba6af0134846
Alisson <alisson.dorneles18@gmail.com>
2026-06-24 20:19:29 -0300

    iniciando a maquina de estados e corrigindo as flags e os ticks(contadores)

--- Core/Inc/maquina_estados.h
+++ Core/Inc/maquina_estados.h
@@ -4,16 +4,18 @@
 #include "main.h"
 #include <stdint.h>
 
-
+// Definição dos 7 Estados da FSM
 #define ESTADO_DESLIGADO   0
 #define ESTADO_INERCIA     1
 #define ESTADO_ACELERANDO  2
 #define ESTADO_FREANDO     3
-#define ESTADO_CRUZEIRO    4
-#define ESTADO_EMERGENCIA  5
+#define ESTADO_AUTOMATICO  4
+#define ESTADO_AQUECIMENTO 5
+#define ESTADO_EMERGENCIA  6
 
-
+// Protótipos das funções
 void Inicializar_Maquina_Estados(void);
 uint8_t Obter_Estado_Atual(void);
+void Atualizar_Maquina_Estados(void);
 
-#endif 
+#endif
--- Core/Src/maquina_estados.c
+++ Core/Src/maquina_estados.c
@@ -1,12 +1,49 @@
 #include "maquina_estados.h"
+#include "central_dados.h"
+#include "tracao.h"
 
 static uint8_t estado_atual;
 
 void Inicializar_Maquina_Estados(void) {
-
+    // Inicia o sistema no estado Desligado
     estado_atual = ESTADO_DESLIGADO;
 }
 
 uint8_t Obter_Estado_Atual(void) {
     return estado_atual;
+}
+
+// O CÉREBRO: Chamado continuamente pelo while(1) do main
+void Atualizar_Maquina_Estados(void) {
+    
+    switch (estado_atual) {
+        
+        case ESTADO_DESLIGADO:
+            
+            break;
+
+        case ESTADO_INERCIA:
+            // Lógica futura: Decaimento suave de 4% a cada 1s
+            break;
+
+        case ESTADO_ACELERANDO:
+            // Lógica futura: Incremento de 4% a cada 0.5s
+            break;
+
+        case ESTADO_FREANDO:
+            // Lógica futura: Decremento de 20% a cada 0.5s
+            break;
+
+        case ESTADO_AUTOMATICO:
+            // Lógica futura: Travar PWM atual
+            break;
+
+        case ESTADO_AQUECIMENTO:
+            // Lógica futura: Forçar desligamento por temperatura
+            break;
+
+        case ESTADO_EMERGENCIA:
+            // Lógica futura: Decremento brusco por obstáculo
+            break;
+    }
 }
